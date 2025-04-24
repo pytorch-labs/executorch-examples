f3b430d44f02e3ff85594a82f08b24f288840740
Mergen Nachin <mnachin@meta.com>
2025-04-24 12:59:12 -0400

    Make MPS not the default (#19)
    Let's make MPS not the default but just fallback to portable ops.

--- mv3/python/export.py
+++ mv3/python/export.py
@@ -8,26 +8,40 @@
 import torch
 import torchvision.models as models
 from executorch.backends.apple.coreml.partition import CoreMLPartitioner
-from executorch.backends.apple.mps.partition import MPSPartitioner
-from executorch.exir.backend.backend_details import CompileSpec 
+from executorch.exir import to_edge 
 from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
 from executorch.exir import to_edge_transform_and_lower
-import ssl
-import certifi
 
 
 def main() -> None:
     model = models.mobilenet_v3_small(weights="DEFAULT").eval()
     sample_inputs = (torch.randn(1, 3, 224, 224),)
 
+    et_program_portable = to_edge(
+        torch.export.export(model, sample_inputs),
+    ).to_executorch() 
+
     et_program_coreml = to_edge_transform_and_lower(
         torch.export.export(model, sample_inputs),
         partitioner=[CoreMLPartitioner()],
     ).to_executorch()
 
-    et_program_mps = to_edge_transform_and_lower(
+    
+    # MPS backend doesn't work yet with pip install today.
+    # Currently, it is just falling back to portable ops instead.
+    # 
+    # Please install ExecuTorch from source if you want to run to MPS backend:
+    # 
+    # https://pytorch.org/executorch/main/using-executorch-building-from-source.html#environment-setup
+    # ./install_executorch.sh --pybind mps coreml xnnpack
+    # ./backends/apple/mps/install_requirements.sh
+    # 
+    # et_program_mps = to_edge_transform_and_lower(
+    #   torch.export.export(model, sample_inputs),
+    #   partitioner=[MPSPartitioner([CompileSpec("use_fp16", bytes([True]))])], 
+    # ).to_executorch()
+    et_program_mps = to_edge(
         torch.export.export(model, sample_inputs),
-        partitioner=[MPSPartitioner([CompileSpec("use_fp16", bytes([True]))])], 
     ).to_executorch()
 
     et_program_xnnpack = to_edge_transform_and_lower(
@@ -35,12 +49,13 @@ def main() -> None:
         partitioner=[XnnpackPartitioner()],
     ).to_executorch()
 
-    save_path = "executorch-examples/mv3/apple/ExecuTorchDemo/ExecuTorchDemo/Resources/Models/MobileNet/"
-    with open(save_path+"mv3_coreml_all.pte", "wb") as file:
+    with open("mv3.pte", "wb") as file:
+        et_program_portable.write_to_file(file)
+    with open("mv3_coreml_all.pte", "wb") as file:
         et_program_coreml.write_to_file(file)
-    with open(save_path+"mv3_mps_float16.pte", "wb") as file:
+    with open("mv3_mps_float16.pte", "wb") as file:
         et_program_mps.write_to_file(file)
-    with open(save_path+"mv3_xnnpack_fp32.pte", "wb") as file:
+    with open("mv3_xnnpack_fp32.pte", "wb") as file:
         et_program_xnnpack.write_to_file(file)
 
 
